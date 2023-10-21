204af6146ce2fab589714d12a24eb2bac59cb93d
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-10-21 17:02:12 +0200

    feat(editor): added dummy layout for frames panel

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Editor from './Components/Editor';
 function App() {
   return (
     <>
-      <div className='h-screen p-3'>
+      <div className='p-3'>
         <Card>
           <Editor />
         </Card>
--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,10 +1,23 @@
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
 function Editor() {
     return (
-        <div className="grid grid-cols-[20%_60%_20%] h-full">
-            <div>
-                Frames
+        <div className="grid grid-cols-[20%_60%_20%] gap-1" style={{height: 'calc(100vh - 40px)'}}>
+            <div className='bg-gray-800 rounded p-1 min-h-full'>
+                <ul className='h-full overflow-auto'>
+                    {Array.from({ length: 30 }).map((_, i) => (
+                        <li className="border-b-2 p-1 relative border-black bg-slate-200">
+                            <img src="https://picsum.photos/40/40" className="border inline mr-1"  />
+                            <span> Frame {i} </span>
+                            <span className='absolute right-3 top-3'>
+                                <FontAwesomeIcon icon={faTrash} />
+                            </span>
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <div className="flex flex-col">
+            <div className="bg-gray-800 rounded p-1 flex flex-col">
                 <div className="h-4/5">
                     Viewport
                 </div>
@@ -12,7 +25,7 @@ function Editor() {
                     Sprite Assets
                 </div>
             </div>
-            <div>
+            <div className='bg-gray-800 rounded p-1'>
                 Inspector
             </div>
         </div>
