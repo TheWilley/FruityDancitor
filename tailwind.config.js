2ecdb17828c000d3a46e3c203c080a2bc9372505
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-10-23 17:26:19 +0200

    feat: added daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
+
 export default {
   content: [
     './index.html',
@@ -30,6 +33,6 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
 
