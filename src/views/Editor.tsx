044b5320ccaa4ac3ed4f832ac45292bebd696a20
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-10-23 18:14:36 +0200

    feat(editor): created dummy layout for inspector

--- src/views/Editor.tsx
+++ src/views/Editor.tsx
@@ -2,8 +2,12 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CardL1 from '../Components/CardL1';
 import Navbar from './Navbar';
+import { List, arrayMove } from 'react-movable';
+import { useState } from 'react';
 
 function Editor() {
+    const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
+
     return (
         <div>
             <Navbar />
@@ -34,7 +38,35 @@ function Editor() {
                     </CardL1>
                 </div>
                 <CardL1>
-                    Inspector
+                    <div className='flex justify-center'>
+                        <img width='150' height='150' src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngmart.com%2Ffiles%2F4%2FCool-PNG-Transparent-Image.png&f=1&nofb=1&ipt=d43a327115815008df0c98c1f14279f2da63b7957477e18da757cae735be6e41&ipo=images' />
+                    </div>
+                    <div className='pt-3'>
+                        <div className='grid grid-cols-2 items-center bg-base-200 p-2 rounded mb-1'>
+                            <div>ID:</div>
+                            <div>xxxxxxx</div>
+                        </div>
+                        <div className='grid grid-cols-2 items-center bg-base-200 p-2 rounded mb-1'>
+                            <div>Name:</div>
+                            <div><input className='input w-full' type='text' /></div>
+                        </div>
+                        <div className='grid grid-cols-2 items-center bg-base-200 p-2 rounded mb-1'>
+                            <div>Renderd:</div>
+                            <div><input className='input w-full' type='text' /></div>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className='text-2xl font-bold'> Frames </h2>
+                        <input type="file" className="file-input file-input-bordered file-input-md w-full max-w-xs mb-2" />
+                        <List
+                            values={items}
+                            onChange={({ oldIndex, newIndex }) =>
+                                setItems(arrayMove(items, oldIndex, newIndex))
+                            }
+                            renderList={({ children, props }) => <ul {...props}>{children}</ul>}
+                            renderItem={({ value, props }) => <li className='bg-base-200 p-2 rounded mb-1' {...props}>{value}</li>}
+                        />
+                    </div>
                 </CardL1>
             </div>
         </div>
