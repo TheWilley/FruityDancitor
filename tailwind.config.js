a5ec1859255ca31b241236fdbff8d81f96fd58ab
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-10-23 17:42:41 +0200

    dev: removed some color themes from tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,24 +13,6 @@ export default {
         light: '#f1f5f8',
         dark: '#121212',
       },
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: {
-        light: '#1f2937',
-        dark: '#1c1c1c',
-      },
-      gray: {
-        light: '#374151',
-        dark: '#686871',
-      },
-      white: {
-        light: '#ffffff',
-        dark: '#F5F5F5',
-      },
-      blue: {
-        light: '#3b82f6',
-        dark: '#0F65BD',
-      }
     },
   },
   plugins: [daisyui],
