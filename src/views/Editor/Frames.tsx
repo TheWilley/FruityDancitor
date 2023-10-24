e583c1848797c012a329e99b4241608e2664b9c4
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-10-24 18:52:11 +0200

    feat(editor): added group tabs to frames card

--- src/views/Editor/Frames.tsx
+++ src/views/Editor/Frames.tsx
@@ -1,17 +1,57 @@
+import { useState } from 'react';
 import CardL1 from '../../Components/CardL1';
 import Frame from './Frame';
 
-function Frames() {
+function FramesList() {
     return (
-        <CardL1 className='min-h-full p-1'>
-            <ul className='h-full overflow-auto'>
-                {Array.from({ length: 30 }).map((_, i) => (
-                    <li>
-                        <Frame index={i} />
-                    </li>
-                ))}
-            </ul>
-        </CardL1>
+        <ul className='h-full overflow-auto'>
+            {Array.from({ length: 30 }).map((_, i) => (
+                <li>
+                    <Frame index={i} />
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+function GroupList() {
+    return (
+        <ul className='h-full overflow-auto'>
+            {Array.from({ length: 10 }).map((_, i) => (
+                <li>
+                    <Frame index={i} />
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+function Frames() {
+    const [tab, setTab] = useState(0);
+
+    const tabs = [
+        {
+            name: 'Frames',
+            view: <FramesList />,
+        },
+        {
+            name: 'Groups',
+            view: <GroupList />,
+        },
+    ];
+    return (
+        <>
+            <CardL1 className='min-h-full p-1'>
+                <div className="tabs">
+                    {tabs.map((item, index) => (
+                        <div className={`tab tab-lifted ${index === tab && 'tab-active'}`} onClick={() => setTab(index)}> {item.name} </div>
+                    ))}
+                </div>
+                <div>
+                    {tabs[tab].view}
+                </div>
+            </CardL1>
+        </>
     );
 }
 
--- src/views/Navbar.tsx
+++ src/views/Navbar.tsx
@@ -53,7 +53,6 @@ function Navbar() {
                 </div>
             </div>
         </>
-
     );
 }
 
