84f7053c0cd048ab3d38bbec5da8d11f31b6a1f6
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-10-26 10:01:31 +0200

    feat(editor): removed tabs in frames card

--- src/views/Editor/components/Frames.tsx
+++ src/views/Editor/components/Frames.tsx
@@ -1,18 +1,5 @@
-import { useState } from 'react';
 import CardL1 from '../../../components/CardL1';
 import Frame from './Frame';
-
-function FramesList() {
-    return (
-        <ul className='h-full overflow-auto'>
-            {Array.from({ length: 30 }).map((_, i) => (
-                <li>
-                    <Frame index={i} />
-                </li>
-            ))}
-        </ul>
-    );
-}
 
 function GroupList() {
     return (
@@ -27,29 +14,10 @@ function GroupList() {
 }
 
 function Frames() {
-    const [tab, setTab] = useState(0);
-
-    const tabs = [
-        {
-            name: 'Frames',
-            view: <FramesList />,
-        },
-        {
-            name: 'Groups',
-            view: <GroupList />,
-        },
-    ];
     return (
         <>
             <CardL1 className='min-h-full p-1'>
-                <div className="tabs">
-                    {tabs.map((item, index) => (
-                        <div className={`tab tab-lifted ${index === tab && 'tab-active'}`} onClick={() => setTab(index)}> {item.name} </div>
-                    ))}
-                </div>
-                <div>
-                    {tabs[tab].view}
-                </div>
+                <GroupList />
             </CardL1>
         </>
     );
