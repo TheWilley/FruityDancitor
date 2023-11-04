34ee35093bb611378ca934b4fcbc0a3c93d1ef9b
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-04 18:10:46 +0100

    refactor(editor): created a hook for preview

--- src/views/Editor/components/Preview.tsx
+++ src/views/Editor/components/Preview.tsx
@@ -1,55 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
+import usePreview from '../../../hooks/UsePreview';
+
 
 function Preview(props: { originalCanvas: HTMLCanvasElement | undefined, selectedRow: number, width: number, height: number }) {
 	const previewRef = useRef(null);
-	const [keepTimer, setKeepTimer] = useState(0);
-	const [currentFrame, setCurrentFrame] = useState(0);
-	const [sx, setSx] = useState(0);
-
-	// Rendering a frame
-	const nextFrame = (context: CanvasRenderingContext2D) => {
-		// Create params
-		const drawParams = {
-			sourceX: sx,
-			sourceY: props.height * (props.selectedRow + 1) - props.height,
-			sourceWidth: props.width,
-			sourceHeight: props.height,
-			destX: 0,
-			destY: 0,
-			destWidth: 100,
-			destHeight: 100,
-		};
-
-		// If context exists, clear and show relevant frame
-		if (context) {
-			context.clearRect(0, 0, 100, 100);
-			context.drawImage(props.originalCanvas!, drawParams.sourceX, drawParams.sourceY, drawParams.sourceWidth, drawParams.sourceHeight, drawParams.destX, drawParams.destY, drawParams.destWidth, drawParams.destHeight);		}
-
-		setSx((sx + props.width > props.width * 7) ? 0 : (sx + props.width));
-		setCurrentFrame(Math.floor(sx / props.width + 1));
-	};
-
-	useEffect(() => {
-		// Check if both canvases exist before continuing 
-		if (props.originalCanvas && previewRef.current) {
-			// Get context
-			const context = (previewRef.current as HTMLCanvasElement).getContext('2d');
-
-			// If context does not exist, return
-			if(!context) return;
-
-			// So that images does not appear blury
-			context.imageSmoothingEnabled = false;
-
-			// Create a timer which keeps timer going and updates frames
-			const timer = setTimeout(() => {
-				setKeepTimer(keepTimer + 1);
-				nextFrame(context);
-			}, 500);
-
-			return () => clearTimeout(timer);
-		}
-	}, [keepTimer]);
+	const [currentFrame] = usePreview(previewRef.current!, props.originalCanvas, props.selectedRow, props.width, props.height);
 
 	return (
 		<div className='flex justify-center'>
