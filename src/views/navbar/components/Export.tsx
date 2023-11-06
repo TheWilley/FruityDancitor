6c26dbd5997a55680f688cf84df43bb0f3266676
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-06 19:36:05 +0100

    style(navbar): changed export layout

--- src/views/navbar/components/Export.tsx
+++ src/views/navbar/components/Export.tsx
@@ -1,7 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IExportSettings } from '../../../global/types';
 import { donwload } from '../../../utils/download';
-import { faFileText } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 function Export(props: { exportSettings: IExportSettings }) {
@@ -9,12 +7,7 @@ function Export(props: { exportSettings: IExportSettings }) {
 
     return (
         <div>
-            <div className="join">
-                <div className='bg-base-200 join-item p-1 pl-2 pr-2'> <FontAwesomeIcon icon={faFileText} /> </div>
-                <div className='w-full mb-2'>
-                    <input type="text" className="input input-bordered join-item input-sm w-full" value={fileName} onChange={(e) => setFileName(e.target.value)} />
-                </div>
-            </div>
+            <input type="text" className="input input-bordered mb-1 input-sm w-full text-center" value={fileName} onChange={(e) => setFileName(e.target.value)} placeholder='File Name...' />
             <button className="btn btn-outline btn-success w-full" onClick={() => donwload(props.exportSettings, fileName)}> Download </button>
         </div>
     );
