a97191965b0499e6b70c19323fb3f91b997b0fff
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-08 20:16:32 +0100

    feat(navbar): added tooltips to settings

--- src/views/navbar/components/AppSettings.tsx
+++ src/views/navbar/components/AppSettings.tsx
@@ -7,7 +7,7 @@ function AppSettings(props: {appSettings: IAppSettings}) {
         <div className='grid grid-cols-3 gap-2'>
             <div className="join">
                 <div className='bg-base-200 join-item p-1 pl-2 pr-2'> <FontAwesomeIcon icon={faPercent} /> </div>
-                <div className='w-full'>
+                <div className='w-full tooltip tooltip-bottom' data-tip="Compression Ratio - Effects the resolution when uploading images">
                     <input type="number" className="input input-bordered join-item input-sm w-full" placeholder='Rows' min={0.1} max={1} step={0.1} value={props.appSettings.compressionRatio} onChange={(e) => props.appSettings.setCompressionRatio(Number(e.target.value))}/>
                 </div>
             </div>
--- src/views/navbar/components/EditorSettings.tsx
+++ src/views/navbar/components/EditorSettings.tsx
@@ -7,19 +7,19 @@ function EditorSettings(props: {editorSettings: IEditorSettings}) {
         <div className='grid grid-cols-3 gap-2'>
             <div className="join">
                 <div className='bg-base-200 join-item p-1 pl-2 pr-2'> <FontAwesomeIcon icon={faBars} /> </div>
-                <div className='w-full'>
+                <div className='w-full tooltip tooltip-bottom' data-tip="Rows - The amount of rows">
                     <input type="number" className="input input-bordered join-item input-sm w-full" placeholder='Rows' min={1} value={props.editorSettings.rows} onChange={(e) => props.editorSettings.setRows(Number(e.target.value))}/>
                 </div>
             </div>
             <div className="join">
                 <div className='bg-base-200 join-item p-1 pl-2 pr-2'> <FontAwesomeIcon icon={faTextWidth} /> </div>
-                <div className='w-full'>
+                <div className='w-full tooltip tooltip-bottom' data-tip="Width - The width of every cell">
                     <input type="number" className="input input-bordered join-item input-sm w-full" placeholder='Width' min={50} value={props.editorSettings.width} onChange={(e) => props.editorSettings.setWidth(Number(e.target.value))} />
                 </div>
             </div>
             <div className="join">
                 <div className='bg-base-200 join-item p-1 pl-2 pr-2'> <FontAwesomeIcon icon={faTextHeight} /> </div>
-                <div className='w-full'>
+                <div className='w-full tooltip tooltip-bottom' data-tip="Height - The height of every cell">
                     <input type="number" className="input input-bordered join-item input-sm w-full" placeholder='Height' min={50} value={props.editorSettings.height} onChange={(e) => props.editorSettings.setHeight(Number(e.target.value))} />
                 </div>
             </div>
