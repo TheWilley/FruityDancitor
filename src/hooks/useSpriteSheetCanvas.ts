8c2a0f50857c1c8eaf4c41fe81179fd4fe5a97f2
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-09 14:01:56 +0100

    refactor: moved drawImageOnTile function to the useSpriteSheetCanvas hook as it was the only section to use the function

--- src/hooks/useSpriteSheetCanvas.ts
+++ src/hooks/useSpriteSheetCanvas.ts
@@ -1,6 +1,17 @@
 import { RefObject, useEffect } from 'react';
 import { IFrame } from '../global/types';
-import { drawImageOnTile } from '../utils/canvas';
+
+/**
+ * Draws an image on a given tile
+ */
+function drawImageOnTile(ctx: CanvasRenderingContext2D, base64: string, y: number, x: number, height: number, width: number, scale:number, xoffset: number, yoffset:number) {
+    const image = new Image();
+    image.src = base64;
+
+    // Draw a rectangle at positon
+    ctx.drawImage(image, x * width * scale + xoffset, y * height * scale + yoffset, width * scale, height * scale);
+    ctx.fill();
+}
 
 export default function useCanvas(canvasRef: RefObject<HTMLCanvasElement>, rows: number, height: number, width: number, frames: IFrame[], setCanvas: React.Dispatch<React.SetStateAction<HTMLCanvasElement | undefined>>) {
     useEffect(() => {
