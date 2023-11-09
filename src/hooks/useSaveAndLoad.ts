70ec3665f649ac1011bc8df47d7a76b5fef58dcf
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-09 20:32:17 +0100

    feat(navbar): added basic file loader for saved configuration file

--- src/hooks/useSaveAndLoad.ts
+++ src/hooks/useSaveAndLoad.ts
@@ -11,8 +11,29 @@ export type ISaveAndLoadSettings = {
 /**
  * Loads a Fruity Dance Generator JSON file
  */
-function load() {
-    console.log('load');
+function load(file: File) {
+    if (file.type === 'application/json') {
+        const reader = new FileReader();
+
+        reader.onloadend = function () {
+            const data = JSON.parse(reader.result as string);
+            if(data.type !== 'fruity_dance_generator_config') {
+                new Error('Not a valid Fruity_Dance_Generator config file');
+                alert('Not a valid Fruity_Dance_Generator config file');
+            } else {
+                alert('loaded!');
+            }
+        };
+
+        reader.onerror = function () {
+            new Error('Failed to read the file');
+            alert('Failed to read the file');
+        };
+
+        reader.readAsText(file);
+    } else {
+        alert('Not a valid sprite sheet file');
+    }
 }
 
 /**
@@ -21,6 +42,7 @@ function load() {
 function save(frames: IFrame[], rows: number, width: number, height: number) {
     // Create a object to collect data (empty rows are removed from JSON)
     const json = {
+        type: 'fruity_dance_generator_config',
         frames: JSON.stringify(frames.filter(item => item.row.length > 0)),
         rows: rows,
         width: width,
--- src/views/editor/components/NavbarSaveAndLoadTab.tsx
+++ src/views/editor/components/NavbarSaveAndLoadTab.tsx
@@ -1,13 +1,16 @@
+import { useRef } from 'react';
 import useSaveAndLoad from '../../../hooks/useSaveAndLoad';
 import { ISaveAndLoadSettings } from '../../../utils/settingsHelper';
 
-function NavbarSaveAndLoadTab(props: {saveAndLoadSettings: ISaveAndLoadSettings}) {
+function NavbarSaveAndLoadTab(props: { saveAndLoadSettings: ISaveAndLoadSettings }) {
     const [save, load] = useSaveAndLoad();
-    const {frames, rows, width, height} = props.saveAndLoadSettings;
+    const { frames, rows, width, height } = props.saveAndLoadSettings;
+    const fileRef = useRef<HTMLInputElement | null>(null);
 
     return (
         <div>
-            <button className="btn btn-outline btn-warning w-full" onClick={() => load()}> Load </button>
+            <input id='selectImage' type="file" onChange={(e) => { e.target.files && load(e.target.files[0]); }} ref={fileRef} className='hidden' />
+            <button className="btn btn-outline btn-warning w-full" onClick={() => fileRef.current && fileRef.current.click()}> Load </button>
             <button className="btn btn-outline btn-success w-full mt-1" onClick={() => save(frames, rows, width, height)}> Save </button>
         </div>
     );
