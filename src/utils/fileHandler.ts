2a698236895662c1c30c0b0bbcefb3fdfdb90512
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-09 13:59:28 +0100

    refactor: moved download logic to fileHandler

--- src/utils/fileHandler.ts
+++ src/utils/fileHandler.ts
@@ -1,4 +1,7 @@
 import Compressor from 'compressorjs';
+import { saveAs } from 'file-saver';
+import JSZip from 'jszip';
+import { IExportSettings } from './settingsHelper';
 
 /**
  * Extract base64 from an image
@@ -28,4 +31,35 @@ function getBase64(file: File, compressionRatio: number) {
     });
 }
 
-export { getBase64 };
+function downloadFile(exportSettings: IExportSettings, filename: string) {
+    // If a filename is not entered, use default name
+    if (!filename) filename = 'spiriteSheet';
+
+    // Check that required settings exists before continue
+    if (!exportSettings.canvas || !exportSettings.frames) return;
+
+    // Create new zip instance
+    const zip = new JSZip();
+
+    // Get row names
+    const rowNames = exportSettings.frames.map((item) => item.name);
+
+    // Convert canvas to image
+    const image = new Image();
+    image.src = exportSettings.canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+
+    // Add files
+    zip.file(`${filename}.txt`, rowNames.join('\n'));
+    zip.file(
+        `${filename}.png`,
+        image.src.substring(image.src.indexOf(',') + 1),
+        { base64: true }
+    );
+
+    zip.generateAsync({ type: 'blob' }).then(function (content) {
+        // see FileSaver.js
+        saveAs(content, `${filename}.zip`);
+    });
+}
+
+export { getBase64, downloadFile };
--- src/views/navbar/components/Export.tsx
+++ src/views/navbar/components/Export.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { download } from '../../../utils/download';
+import { downloadFile } from '../../../utils/fileHandler';
 import { IExportSettings } from '../../../utils/settingsHelper';
 
 function Export(props: { exportSettings: IExportSettings }) {
@@ -8,7 +8,7 @@ function Export(props: { exportSettings: IExportSettings }) {
     return (
         <div>
             <input type="text" className="input input-bordered mb-1 input-sm w-full text-center" value={fileName} onChange={(e) => setFileName(e.target.value)} placeholder='File Name...' />
-            <button className="btn btn-outline btn-success w-full" onClick={() => download(props.exportSettings, fileName)}> Download </button>
+            <button className="btn btn-outline btn-success w-full" onClick={() => downloadFile(props.exportSettings, fileName)}> Download </button>
         </div>
     );
 }
