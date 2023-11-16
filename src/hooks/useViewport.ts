ff721db3c7c2de249bb1218142c31730b7792b1d
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-16 13:16:00 +0100

    fix(editor): fixed styling bug where parts of the viewport would be hidden if it was too large
    for example, the top part of the viewport was never visible if the height was too large

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -43,7 +43,8 @@ export default function useViewport(canvasRef: RefObject<HTMLCanvasElement>, num
     });
 
     const className = [
-        'bg-[url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.nWLpYSGP33IYGhcR1sFOHgAAAA%26pid%3DApi&f=1&ipt=5812f5c126591b3cde8929ba6262c2374c2a488462b03474da6bd2da7c3a5bab&ipo=images)]'
+        'bg-[url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.nWLpYSGP33IYGhcR1sFOHgAAAA%26pid%3DApi&f=1&ipt=5812f5c126591b3cde8929ba6262c2374c2a488462b03474da6bd2da7c3a5bab&ipo=images)]',
+        'overflow-auto'
     ].join(' ');
 
     return [width * 8, height * numberOfSequences, className] as const;
