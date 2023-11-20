4a868e4811d5bf91391533df027a66314b035adb
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-20 14:05:51 +0100

    feat(navbar): added logotype to info tab

--- src/views/editor/components/navbar/NavbarInfoTab.tsx
+++ src/views/editor/components/navbar/NavbarInfoTab.tsx
@@ -1,13 +1,20 @@
+import logotype from '../../../../media/logotype.svg';
+
 export default function NavbarInfoTab() {
     return (
         <div className="ml-2">
-            <div className="text-2xl font-bold">
-                FruityDancitor
-                <div className='text-sm'>
-                    By TheWilley
+            <div className="flex items-center">
+                <img src={logotype} alt='logotype' width='120' height='120' className="mr-2" />
+                <div>
+                    <div className="text-2xl font-bold">
+                        FruityDancitor
+                    </div>
+                    <div className='text-sm'>
+                        By TheWilley
+                    </div>
                 </div>
             </div>
-            <ul className="flex mt-2 [&>*]:mr-3">
+            <ul className="flex mt-2 [&>*]:mr-3 ml-4">
                 <li className="card">
                     <a href="https://github.com/TheWilley/FruityDancitor" target="_blank" className="link"> Source
                         Code </a>
