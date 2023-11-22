1341122c092febecedb67f77bda57dbed4e80a85
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-11-22 15:48:47 +0100

    feat: added 'eslint-plugin-jsdoc' package and equivelent rules in eslint to enforce comments on functions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,18 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsdoc/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsdoc'],
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
     'semi': 2,
     'quotes': [1, 'single'],
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
+    'jsdoc/require-returns': 'off',
+    "jsdoc/require-param": 'off',
   },
 };
