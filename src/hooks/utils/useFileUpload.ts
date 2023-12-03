6d5c2c2182c8a83828719a50289d7ad69687ff64
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-12-03 20:37:41 +0100

    fix: fixed bug where frames could be uploaded to the wrong sequence
    this was due to a useCallback function which did has missing deps

--- src/hooks/utils/useFileUpload.ts
+++ src/hooks/utils/useFileUpload.ts
@@ -78,7 +78,7 @@ export default function useFileUpload(
         }
       }
     },
-    [spriteSheetFrames]
+    [addNewFrame, compressionRatio, selectedSequence, spriteSheetFrames]
   );
 
   // When hovering over
