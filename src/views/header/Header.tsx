8723384b37f12e9a8cb49d6d4a4464d3f32a934b
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-12-10 12:45:57 +0100

    fix: fixed bug where theme dropdown would be hidden behind the editor
    this was due to a position not being set

--- src/views/header/Header.tsx
+++ src/views/header/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
 
   return (
     <div
-      className='navbar bg-base-100 rounded-md mb-2 transition-all'
+      className='navbar bg-base-100 rounded-md mb-2 transition-all relative z-30'
       style={{
         opacity: showHeader ? '100%' : '0%',
         transform: showHeader ? 'translateY(0px)' : 'translateY(-120px)',
