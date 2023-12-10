db762905e45d7c08231ebc04bbe024054468c457
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-12-10 11:04:07 +0100

    style: made background color follow theme

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div id='customBackground' className='z-10 fixed w-screen h-screen bg-cover' />
-      <div className='bg-background-light dark:bg-background-dark p-5 flex justify-center'>
+      <div className='bg-base-300/80 p-5 flex justify-center'>
         <div style={{ maxWidth: appConfig.maxWidth }} className='w-full z-20'>
           <Accordion />
         </div>
