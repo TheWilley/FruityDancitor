50462b62a270aee2b51b4a5d0bc5b57f1efc5834
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-02-24 15:20:19 +0100

    feat: created input component and replaced previous frame mods inputs with it

--- src/views/editor/inspector/InspectorFrameMods.tsx
+++ src/views/editor/inspector/InspectorFrameMods.tsx
@@ -1,5 +1,11 @@
 import useFrameMods from '../../../hooks/utils/useFrameMods.ts';
 import useInputValidation from '../../../hooks/utils/useInputValidation.ts';
+import {
+  faUpRightAndDownLeftFromCenter,
+  faX,
+  faY,
+} from '@fortawesome/free-solid-svg-icons';
+import Input from '../../../components/Input.tsx';
 
 /**
  * Component which represents settings for a selected frame.
@@ -10,13 +16,10 @@ function InspectorFrameMods() {
 
   return (
     <>
-      <div>
-        <label className='label'>
-          <span className='label-text'>Scale</span>
-        </label>
+      <Input faIcon={faUpRightAndDownLeftFromCenter} tooltip='Scale Multiplier'>
         <input
           type='number'
-          className='input input-md input-bordered w-full'
+          className='input join-item input-bordered w-full'
           step={0.1}
           min={0.1}
           max={3}
@@ -24,40 +27,31 @@ function InspectorFrameMods() {
           onChange={(e) => validateNumberInput('float', e, (value) => setScale(value))}
           disabled={disabled}
         />
-      </div>
-      <div className='grid grid-cols-1 gap-3 lg:grid-cols-2'>
-        <div>
-          <label className='label'>
-            <span className='label-text'>X-Offset</span>
-          </label>
-          <input
-            type='number'
-            className='input input-md input-bordered w-full'
-            min={-500}
-            value={mods.xoffset}
-            onChange={(e) =>
-              validateNumberInput('number', e, (value) => setxoffset(value))
-            }
-            disabled={disabled}
-          />
-        </div>
-        <div>
-          <label className='label'>
-            <span className='label-text'>Y-Offset</span>
-          </label>
-          <input
-            type='number'
-            className='input input-md input-bordered w-full'
-            min={-500}
-            value={mods.yoffset}
-            onChange={(e) =>
-              validateNumberInput('number', e, (value) => setyoffset(value))
-            }
-            disabled={disabled}
-          />
-        </div>
-      </div>
-      <button className='btn btn-md mt-4 w-full' onClick={resetMods} disabled={disabled}>
+      </Input>
+
+      <Input faIcon={faX} tooltip='X-Offset'>
+        <input
+          type='number'
+          className='input join-item input-bordered w-full'
+          min={-500}
+          value={mods.xoffset}
+          onChange={(e) => validateNumberInput('number', e, (value) => setxoffset(value))}
+          disabled={disabled}
+        />
+      </Input>
+
+      <Input faIcon={faY} tooltip='Y-Offset'>
+        <input
+          type='number'
+          className='input join-item input-bordered w-full'
+          min={-500}
+          value={mods.yoffset}
+          onChange={(e) => validateNumberInput('number', e, (value) => setyoffset(value))}
+          disabled={disabled}
+        />
+      </Input>
+
+      <button className='btn btn-md w-full' onClick={resetMods} disabled={disabled}>
         Reset
       </button>
     </>
