23b2875a22476a70138380dfdffe667810c1fc00
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-02-24 13:08:22 +0100

    feat: leading 0's are now removed in number inputs

--- src/hooks/utils/useInputValidation.ts
+++ src/hooks/utils/useInputValidation.ts
@@ -7,22 +7,27 @@ export default function useInputValidation() {
   const validateNumberInput = (
     type: 'number' | 'float',
     event: ChangeEvent<HTMLInputElement>,
-    callback: (value: number) => void
+    callback: (value: string) => void
   ) => {
     event.preventDefault();
-    const { value, min, max } = event.target;
+    const { value, min, max, defaultValue } = event.target;
 
     // Parse all values
     const parsedValue = type === 'float' ? parseFloat(value) : parseInt(value);
     const parsedMin = min ? (type === 'float' ? parseFloat(min) : parseInt(min)) : null;
     const parsedMax = max ? (type === 'float' ? parseFloat(max) : parseInt(max)) : null;
+    const parsedDefault = type === 'float' ? parseFloat(defaultValue) : parseInt(defaultValue);
 
-    // Makes sure we don't go over set values
-    if (
-      (parsedMin === null || parsedValue >= parsedMin) &&
-      (parsedMax === null || parsedValue <= parsedMax)
-    ) {
-      callback(parsedValue);
+    if (isNaN(parsedValue)) {
+      callback(parsedDefault.toString());
+    } else {
+      // Makes sure we don't go over set values
+      if (
+        (parsedMin === null || parsedValue >= parsedMin) &&
+        (parsedMax === null || parsedValue <= parsedMax)
+      ) {
+        callback(parsedValue.toString());
+      }
     }
   };
 
